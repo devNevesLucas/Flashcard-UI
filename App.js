769d1472a247a370497dd9f8f4f646d0555514d9
Lucas Neves <lzmn70@gmail.com>
2025-05-15 10:06:44 -0300

    Remoção do SafeAreaProvider
    temporário

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthNavigator from './navigator/AuthNav';
 import MainNavigator from './navigator/MainNav';
 import { useState } from 'react';
@@ -11,14 +11,19 @@ export default function App() {
   const [userLogado, setUserLogado] = useState(false);
 
   return (
-    <SafeAreaProvider style={styles.container}>
+    
       <NavigationContainer>
         {userLogado? <MainNavigator /> : <AuthNavigator /> }
       </NavigationContainer>
-    </SafeAreaProvider>
+    
   );
 }
 
+
+/*
+<SafeAreaProvider style={styles.container}>
+</SafeAreaProvider>
+*/
 
 const styles = StyleSheet.create({
   container: {
